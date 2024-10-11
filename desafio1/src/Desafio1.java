b09762f1cbdca860daf9e62f27658b72eb8c1f3f
mcosta-df <mcosta.df@gmail.com>
2024-10-10 22:08:13 -0300

    Implementacao desafio 1

---
+++ desafio1/src/Desafio1.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        numeros.sort((num1, num2) -> num1.compareTo(num2));
+        numeros.forEach(System.out::println);
+    }
+}
