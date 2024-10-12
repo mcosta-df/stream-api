cea450fb65de16ff92f913fbe3a1e89d21edaed5
mcosta-df <mcosta.df@gmail.com>
2024-10-11 22:25:17 -0300

    Desafio 5 ajuste

--- desafio5/src/Desafio5.java
+++ desafio5/src/Desafio5.java
@@ -6,7 +6,7 @@ public class Desafio5
     public static void main(String[] args) throws Exception {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-            double media = numeros.stream().mapToDouble(Integer::doubleValue).average().orElse(0.0);
+            double media = numeros.stream().filter(n -> n > 5).mapToDouble(Integer::doubleValue).average().orElse(0.0);
             System.out.println(media);
         }
 }
